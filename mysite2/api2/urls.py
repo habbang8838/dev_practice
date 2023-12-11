131e177d96ae3f91285b5920975d7061e8b086f2
jisoopark <05pjs0103@naver.com>
2023-12-11 09:32:51 +0900

    aa

---
+++ mysite2/api2/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path, include
+from rest_framework import routers
+from . import views
+
+# router = routers.DefaultRouter()
+# router.register('user', UserViewSet)
+# router.register('post', PostViewSet)
+# router.register('comment', CommentViewSet)
+
+# urlpatterns = [
+#     path('', include(router.urls)),
+# ]
+
+urlpatterns = [
+    path('post/', views.PostListAPIView.as_view(), name='post-list'),
+    path('post/<int:pk>/', views.PostRetrieveAPIView.as_view(), name='post-detail'),
+    path('comment/', views.CommentCreateAPIView.as_view(), name='comment-list'),
+    path('post/<int:pk>/like/', views.PostLikeAPIView.as_view(), name='post-like'),
+    path('catetag/', views.CateTagAPIView.as_view(), name='catetag'),
+]
